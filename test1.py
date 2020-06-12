234e477bcc47d2ebe32f5b2ee385348b5098c638
Esen <esenkharkov@gmail.com>
2020-06-12 12:49:35 +0300

    decimal.context update

--- test1.py
+++ test1.py
@@ -1,6 +1,10 @@
 import decimal
 
-with decimal.localcontext() as ctx:
-    ctx.prec = 2;
-    print(decimal.Decimal('1.00') / decimal.Decimal('3.00'))
+try:
+    with decimal.localcontext() as ctx:
+        ctx.prec = 2;
+        print(decimal.Decimal(1.00) / decimal.Decimal('3.00'))
+        print(1/0)
+except Exception as e:
+    print(e)
 print(decimal.Decimal('1.00') / decimal.Decimal('3.00'))
